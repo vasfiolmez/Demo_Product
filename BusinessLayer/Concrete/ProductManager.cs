73278b35fb835b90bba1dd69c7ba1b31ae08b03c
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2023-05-29 23:59:38 +0300

    businessLayer

---
+++ BusinessLayer/Concrete/ProductManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            productDal = _productDal;
+        }
+
+        public List<Product> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Product t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Product t)
+        {
+            
+        }
+
+        public void TUpdate(Product t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
