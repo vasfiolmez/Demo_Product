7bfe9e367ad8da65a4a73b196dbd57b9fca9cf7c
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2023-06-23 01:42:50 +0300

    register

---
+++ Demo_Product/Controllers/RegisterController.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_Product.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
